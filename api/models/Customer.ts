595469e78ce89ddbb1e67b957b95e42a5f6d4680
Julija-Damjanovic <julija.damjanovic.matf@gmail.com>
2023-10-31 18:08:57 +0100

    promene

--- api/models/Customer.ts
+++ api/models/Customer.ts
@@ -19,9 +19,6 @@ const Customer = connection.define(
     },
       password: {
         type: DataTypes.STRING(64),
-        validate: {
-          is: /^[0-9a-f]{64}$/i
-        },
       },
   },
   {
