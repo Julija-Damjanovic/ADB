b570c7c10af34827a6b7fad81fcd0ff8b111bb96
Julija-Damjanovic <julija.damjanovic.matf@gmail.com>
2023-11-23 11:31:06 +0100

    add route

--- api/customers/index.ts
+++ api/customers/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import ProductsRoutes from './products';
 import MakersRoutes from './makers';
+import CustomerStamps from './customersStamps';
 
 const router = Router();
 
 router.use('/products', ProductsRoutes);
 router.use('/makers', MakersRoutes);
+router.use('/customersStamps', CustomerStamps);
 // add other routes...
 
 export default router;
