0f61bd71d87c173c7bbda8d47079270826d9af3c
Julija-Damjanovic <julija.damjanovic.matf@gmail.com>
2023-11-23 14:20:53 +0100

    fixed route

--- api/index.ts
+++ api/index.ts
@@ -7,9 +7,9 @@ import ProfileRoutes from "./public/customers/profile"
 import CustomersStampsRoutes from "./public/customersStamps";
 const router = Router();
 
-router.use('/users', UserRoutes);
+router.use('/users',verifyToken, UserRoutes);
 router.use('/public', PublicRoutes);
-router.use('/customers', CustomerRoutes);
+router.use('/customers', verifyToken,CustomerRoutes);
 router.use('/customers/profile', verifyToken, ProfileRoutes);
 router.use('/customersStamps', CustomersStampsRoutes);
 // add other routes...
