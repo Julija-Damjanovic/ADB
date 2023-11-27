81b30b67fae293d05f53a59467220ada5e2a4a3d
Julija-Damjanovic <julija.damjanovic.matf@gmail.com>
2023-11-27 14:06:24 +0100

    modified route /users/makers

--- api/users/makers/index.ts
+++ api/users/makers/index.ts
@@ -5,55 +5,56 @@ import Maker from "../../models/Maker";
 
 const router = Router();
 
-router.get("/", async(req: request, res: response) => {
-   const data = await Maker.findAll();
-     
-   res.json({
+router.get("/", async (req: request, res: response) => {
+  const data = await Maker.findAll();
+
+  res.json({
     message: "Welcome to API USERS MAKERS ALL",
-    result : data,
+    result: data,
   });
 });
 
-router.get("/:id", async(req: request, res: response) => {
+router.get("/:id", async (req: request, res: response) => {
   const data = await Maker.findOne({
-where: { id:req.params.id},
-//include:Maker,
+    where: { id: req.params.id },
+    //include:Maker,
   });
-  
+
   res.json({
     message: "Welcome to API USERS MAKERS ID:" + req.params.id,
-    result: data, 
+    result: data,
   });
 });
 
-router.post("/", async(req: request, res: response) => {
-  console.log(req.body);
-  const data = await Maker.create(req.body);
+router.post("/", async (req: request, res: response) => {
+  const { name, theme, free_delivery_min_ammount, VAT_level, is_active, description, comission } = req.body;
+  const data = await Maker.create({ name, theme, free_delivery_min_ammount, VAT_level, is_active, description, comission });
   res.json({
-  result: data,   
+    result: data,
   });
 });
 
-router.put("/:id", async(req: request, res: response) => {
-  const data = await Maker.update(req.body, {
-   where: { id:req.params.id, },
+router.put("/:id", async (req: request, res: response) => {
+  const { name, theme, free_delivery_min_ammount, VAT_level, is_active, description, comission } = req.body;
+
+  const data = await Maker.update({ name, theme, free_delivery_min_ammount, VAT_level, is_active, description, comission }, {
+    where: { id: req.params.id, },
   });
 
   res.json({
-    message: "Welcome to PUT API USERS MAKERS ID:" + req.params.id,
-     result: data,
+    result: data,
   });
 });
 
-router.delete("/:id", async(req: request, res: response) => {
+router.delete("/:id", async (req: request, res: response) => {
   const data = await Maker.destroy({
     where: {
-       id: req.params.id,
+      id: req.params.id,
     },
   });
-  
+
   res.json({
-    message: "Welcome to DELETE API USERS MAKERS ID:" + req.params.id,
+    message: "DELETE API USERS MAKERS ID:" + req.params.id,
     result: data,
   });
 });
