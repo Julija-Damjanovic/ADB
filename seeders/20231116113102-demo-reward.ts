79b18b71cd8b58879da85da0f694c8dc872e7879
Julija-Damjanovic <julija.damjanovic.matf@gmail.com>
2023-11-27 13:38:22 +0100

    modified seeder reward

--- seeders/20231116113102-demo-reward.ts
+++ seeders/20231116113102-demo-reward.ts
@@ -16,11 +16,42 @@ module.exports = {
       is_active: true,
       stamp_ammount_needed: 1000,
       max_use_ammount: 1000,
-      expires: "2023-11-22 10:45:00",
+      expires: "2025-11-22 10:45:00",
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      MakerId:1,
+      DiscountId: 8,
 
-    }], {});
+    },{
+      is_active: true,
+      stamp_ammount_needed: 1500,
+      max_use_ammount: 1500,
+      expires: "2025-11-22 10:45:00",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      MakerId:2,
+      DiscountId: 8,
+    },
+  {
+    is_active: true,
+    stamp_ammount_needed: 5000,
+    max_use_ammount: 5000,
+    expires: "2026-11-22 10:45:00",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    MakerId:3,
+    DiscountId: 7,
+  },
+{
+  is_active: false,
+  stamp_ammount_needed: 2000,
+  max_use_ammount: 2000,
+  expires: "2024-11-22 10:45:00",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  MakerId:4,
+  DiscountId: 7,
+}], {});
 
 
   },
